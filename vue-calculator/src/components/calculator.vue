3cb9615d89bcb199f5b22a06739a38c08b5848fe
Keegan <kkevern23@Xubuntu22.04.myguest.virtualbox.org>
2024-11-11 19:24:08 +0000

    Finished CSS functionaluty, combined click and keyboard input

--- vue-calculator/src/components/calculator.vue
+++ vue-calculator/src/components/calculator.vue
@@ -1,12 +1,15 @@
 <script>
 export default {
   data() {
-    return { current: "", previous: "", operator: "" };
+    return { current: "", previous: "", operator: "", previousOperator: "" };
   },
   methods: {
     clear() {
       this.current = "";
       this.previous = "";
+      document.getElementById(this.operator).classList.remove("!bg-blue-400");
+      this.operator = "";
+      this.previousOperator = "";
     },
     backspace() {
       this.current = this.current.slice(0, -1);
@@ -38,6 +41,10 @@ export default {
         "/": this.divide,
       };
 
+      if (this.operator === "=") {
+        this.operator === this.previousOperator;
+      }
+
       // Check if the operator is valid
       if (operations[operator]) {
         // Perform the operation, round to two decimals, and then apply truncateTrailingZeros
@@ -65,11 +72,11 @@ export default {
       }
     },
 
-    truncateTrailingZeros(a) {
+    truncateTrailingZeros(longDecimalDigit) {
       //If value ends in n.00, floor the value and return n (returns int but can recieve float)
-      if (a % 1 === 0) {
-        return Math.floor(a);
-      } else return a;
+      if (longDecimalDigit % 1 === 0) {
+        return Math.floor(longDecimalDigit);
+      } else return longDecimalDigit;
     },
     numberInput(digit) {
       if (this.current === this.previous) {
@@ -81,20 +88,8 @@ export default {
       if (this.current.length >= 14) {
         this.updateDisplay("Number too long");
       }
-
-      // if (digit === ".") {
-      //   if (this.current.includes(".")) {
-      //   } else {
-      //     this.current = `${this.current}${digit}`;
-      //   }
-      // } else {
-      //   this.current = `${this.current}${digit}`;
-      // }
     },
-    // currentToPrevious() {
-    //   this.previous = parseFloat(this.current);
-    //   this.current = "";
-    // },
+
     currentToPrevious() {
       this.previous = parseFloat(this.current);
     },
@@ -110,17 +105,7 @@ export default {
           //Check to see if number input
           this.numberInput(event.key.toString());
         } else if (operatorArray.includes(event.key)) {
-          //if previous has a value, display the result of the equation, if not update the display to ""
-          if (this.previous !== "") {
-            this.equals(this.operator);
-            this.currentToPrevious();
-          } else {
-            this.currentToPrevious();
-            this.updateDisplay("");
-          }
-
-          //check to see if operator input
-          this.operator = event.key.toString();
+          this.operatorClick(event.key);
         }
 
         switch (event.key.toString()) {
@@ -129,6 +114,7 @@ export default {
             break;
           case "=":
           case "Enter":
+            this.previousOperator = this.operator;
             this.equals(this.operator);
             break;
           case ".":
@@ -136,31 +122,21 @@ export default {
         }
       }
     },
-    operatorClick(decimalAsciiForOperator) {
+    operatorClick(givenOperator) {
       //If an operator has already been pressed we need to continuously calculate
       //I.e. perform equals after every new operator.
       if (this.operator !== "") {
-        console.log("we here");
+        //remove previous operator "in use" colour, should one exist
+
+        document.getElementById(this.operator).classList.remove("!bg-blue-400");
         this.equals(this.operator);
       }
 
       //If an operator has not been entered during this calculation, proceed as normal.
-      switch (decimalAsciiForOperator) {
-        case 42:
-          this.operator = "*";
-          break;
-        case 43:
-          this.operator = "+";
-          break;
-        case 45:
-          this.operator = "-";
-          break;
-        case 47:
-          this.operator = "/";
-          break;
-      }
+      this.operator = givenOperator;
       this.previous = parseFloat(this.current);
       this.current = "";
+      document.getElementById(this.operator).classList.add("!bg-blue-400");
     },
   },
   mounted() {
@@ -172,26 +148,68 @@ export default {
 <template>
   <div class="calculator rounded-md shadow-lg">
     <div class="display text-6xl">{{ current || "0" }}</div>
-    <button @click="operatorClick(43)" class="button operator">+</button>
-    <button @click="operatorClick(45)" class="button operator">-</button>
-    <button @click="operatorClick(42)" class="button operator">x</button>
-    <button @click="operatorClick(47)" class="button operator">/</button>
-    <button @click="numberInput('1')" class="button">1</button>
-    <button @click="numberInput('2')" class="button">2</button>
-    <button @click="numberInput('3')" class="button">3</button>
-    <button @click="backspace" class="button operator"><-</button>
-    <button @click="numberInput('4')" class="button">4</button>
-    <button @click="numberInput('5')" class="button">5</button>
-    <button @click="numberInput('6')" class="button">6</button>
-    <button @click="equals(this.operator)" class="button operator row-span-3">
+    <button
+      @click="operatorClick('+')"
+      id="+"
+      class="button operator"
+      aria-label="plus"
+    >
+      +
+    </button>
+    <button
+      @click="operatorClick('-')"
+      id="-"
+      class="button operator"
+      aria-label="minus"
+    >
+      -
+    </button>
+    <button
+      @click="operatorClick('*')"
+      id="*"
+      class="button operator"
+      aria-label="multiply"
+    >
+      x
+    </button>
+    <button
+      @click="operatorClick('/')"
+      id="/"
+      class="button operator"
+      aria-label="divide"
+    >
+      /
+    </button>
+    <button @click="numberInput('1')" class="button" aria-label="1">1</button>
+    <button @click="numberInput('2')" class="button" aria-label="2">2</button>
+    <button @click="numberInput('3')" class="button" aria-label="3">3</button>
+    <button @click="backspace" class="button operator" aria-label="delete">
+      <-
+    </button>
+    <button @click="numberInput('4')" class="button" aria-label="4">4</button>
+    <button @click="numberInput('5')" class="button" aria-label="5">5</button>
+    <button @click="numberInput('6')" class="button" aria-label="6">6</button>
+    <button
+      @click="equals(this.operator)"
+      class="button operator row-span-3"
+      aria-label="equals"
+    >
       =
     </button>
-    <button @click="numberInput('7')" class="button">7</button>
-    <button @click="numberInput('8')" class="button">8</button>
-    <button @click="numberInput('9')" class="button">9</button>
-    <button @click="numberInput('.')" class="button !bg-gray-200">.</button>
-    <button @click="numberInput('0')" class="button">0</button>
-    <button @click="clear" class="button !bg-gray-200">AC</button>
+    <button @click="numberInput('7')" class="button" aria-label="7">7</button>
+    <button @click="numberInput('8')" class="button" aria-label="8">8</button>
+    <button @click="numberInput('9')" class="button" aria-label="9">9</button>
+    <button
+      @click="numberInput('.')"
+      class="button !bg-gray-200"
+      aria-label="decimal"
+    >
+      .
+    </button>
+    <button @click="numberInput('0')" class="button" aria-label="0">0</button>
+    <button @click="clear" class="button !bg-gray-200" aria-label="clear">
+      AC
+    </button>
   </div>
 </template>
 
@@ -200,7 +218,7 @@ export default {
   width: 100vw;
   height: min-content;
   display: grid;
-  grid-template-rows: 15% 85px 85px 85px 85px 85px;
+  grid-template-rows: 50vh 10vh 10vh 10vh 10vh 10vh;
   grid-template-columns: 25% 25% 25% 25%;
 }
 
@@ -223,6 +241,11 @@ export default {
   border: solid lightgrey 1px;
   border-radius: inherit;
   background-color: orange;
+  transition: transform 0.2s ease, box-shadow 0.2s ease;
+}
+
+.button:active {
+  transform: translate(2px, 2px);
 }
 
 @media (min-width: 769px) {
