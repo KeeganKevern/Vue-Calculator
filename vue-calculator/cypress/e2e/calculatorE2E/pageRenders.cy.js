e7faadc47432ce5195832b1bd50d64b2537f9829
Keegan <keegankevern.work@gmail.com>
2025-02-20 17:26:50 +0000

    set up cypress and initiated basic page rendering tests

---
+++ vue-calculator/cypress/e2e/calculatorE2E/pageRenders.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+//Load appropriate page
+describe("Page rendering", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+    cy.wait(2000);
+  });
+
+  it("check status 200", () => {
+    cy.request("http://localhost:5173").its("status").should("eq", 200);
+  });
+
+  it("buttons loaded", () => {
+    cy.get(".button").should("be.visible");
+    cy.get(".button").should("have.length", 18);
+  });
+
+  it("Current/previous sectioned loaded with correct values", () => {
+    cy.get(".display").should("exist");
+    cy.get(".display").should("be.visible");
+    cy.get(".display").contains("0");
+    cy.get("#previousDisplay").should("exist");
+    cy.get("#previousDisplay").should("be.visible");
+    cy.get("#previousDisplay").should("be.empty");
+  });
+});
